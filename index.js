506ad944a4d6b5a05ef886767cf863176a9befe4
Karen Olson <karmarie@gmail.com>
2021-07-23 14:42:38 -0500

    commit tree 9cd49686c102c8f4246c522bf72e4140f5c8d923

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(startingBlock) {
+    /*if(startingBlock >= 42) {
+        return startingBlock - 42;
+    } else {
+        return 42 - startingBlock;
+    }
+    */
+    return Math.abs(startingBlock - 42);
+} 
+
+function distanceFromHqInFeet(startingBlock) {
+    return distanceFromHqInBlocks(startingBlock) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+    return Math.abs((startingBlock - endingBlock) * 264);
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+    const feet = distanceTravelledInFeet(startingBlock, endingBlock);
+    if (feet > 2500) {
+        return 'cannot travel that far';
+    } else if (feet > 2000) {
+        return 25;
+    } else if (feet > 400) {
+        return (feet - 400) * .02;
+    } else if (feet <= 400) {
+        return 0;
+    }
+}
+
+const price = calculatesFarePrice(50, 58);
+price;
